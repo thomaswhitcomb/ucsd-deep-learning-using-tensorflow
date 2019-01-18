5af8a64f5a4072b865da08fdf203bf5569695e20
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-01-18 04:08:26 +0000

    1 and 2 done

--- lesson1/lesson1.py
+++ lesson1/lesson1.py
@@ -1,1 +1,7 @@
 import pandas as pd
+
+team_list = ["Golden State","49ers","Giants","Cavaliers","Lakers","Rams","Yankees"]
+team_dict = {"Oakland-Basketball" :"Golden State","SF-Football":"49ers","SF-Baseball":"Giants","Cleveland-Basketball":"Cavaliers","LA-Basketball":"Lakers","LA-Football":"Rams","NY-Baseball":"Yankees"}
+
+team_series1 = pd.Series(team_list)
+team_series2 = pd.Series(team_dict)
