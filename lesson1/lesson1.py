85431679fd95eeb77856f966e49af58502005286
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-01-19 06:24:34 +0000

    problem  7

--- lesson1/lesson1.py
+++ lesson1/lesson1.py
@@ -49,3 +49,19 @@ if np.sum(num + 5) == 5550:
   print("Problem #5 is correct")
 else:
   print("Problem #5 is not correct")
+############################################################
+# Problem #6
+############################################################
+d = {"TV":pd.Series([230.1,44.5,17.2,151.5,180.8,8.7,57.5,120.2,8.6],index=[1,2,3,4,5,6,7,8,9]),
+     "Radio":pd.Series([37.8,39.3,45.9,41.3,10.8,48.9,32.8,19.6,2.1],index=[1,2,3,4,5,6,7,8,9]),
+     "NewsPaper":pd.Series([69.2,45.1,69.3,58.5,58.4,75,23.5,11.6,1],index=[1,2,3,4,5,6,7,8,9]),
+     "Sales":pd.Series([22.1,10.4,9.3,18.5,12.9,7.2,11.8,13.2,4.8],index=[1,2,3,4,5,6,7,8,9])}
+pd.DataFrame(d)
+############################################################
+# Problem #7
+############################################################
+df = pd.read_csv("00 kc_house_data.csv")
+print("Number of observations in dataframe is ",(df.shape[0])*df.shape[1])
+print("Average house price is ",df['price'].mean())
+bm = df['price'].gt(500000)
+print("Number of houses which are priced greater than $500,000 is ",df[bm].shape[0])
