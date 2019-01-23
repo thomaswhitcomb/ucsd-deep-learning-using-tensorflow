40c37e5d643a0167f124e33d8774428fabf9f059
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-01-23 02:53:33 +0000

    through problem 3

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -5,16 +5,19 @@ def main():
     tensor_x = tf.constant(list(range(100,110)))
     tensor_y = tf.constant([34, 28, 45, 67, 89, 93, 24, 49, 11, 7])
     tensor_sum = tf.add(tensor_x,tensor_y)
-    print(tensor_sum)
+    print("Problem #1 Lazy",tensor_sum)
     with tf.Session() as sess:
       output = sess.run(tensor_sum)
-      print(output)
-    sess = tf.Session()
-    print(sess.run(tensor_sum))
+      print("Problem #1 Eager",sess.run(tensor_sum))
 
-    a1 = [[[1,1,1,1],[2,2,2,2]],[[1,1,1,1],[2,2,2,2]],[[1,1,1,1],[2,2,2,2]],[[1,1,1,1],[2,2,2,2]] ]
-    s1 = tf.stack(a1,axis=0)
-    print(s1.shape)
+    x1 = tf.constant([[1,2,3,4],[5,6,7,8]])
+    s1 = tf.stack([x1,x1,x1,x1])
+    print("Problem #2",s1.shape)
+
+    x1 = tf.constant([[1,2,3,4],[5,6,7,8]])
+    s1 = tf.stack([x1])
+    print("Problem #3",s1.shape)
+    
 
 if __name__ == "__main__":
     main()
