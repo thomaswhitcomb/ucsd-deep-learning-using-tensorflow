4b87a98be74ecff249247b6de1670a318185e981
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-01-24 19:13:45 +0000

    linting

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -3,59 +3,59 @@ import tensorflow as tf
 class Tensors:
     def __init__(self):
         tf.reset_default_graph()
-        self.a = tf.constant(1.12,name="a")
-        self.b = tf.constant(2.34,name="b")
-        self.c = tf.constant(0.72,name="c")
-        self.d = tf.constant(0.81,name="d")
-        self.f = tf.constant(19.83,name="f")
+        self.a = tf.constant(1.12, name="a")
+        self.b = tf.constant(2.34, name="b")
+        self.c = tf.constant(0.72, name="c")
+        self.d = tf.constant(0.81, name="d")
+        self.f = tf.constant(19.83, name="f")
 
-    def x(self):
-         return (tf.add(tf.constant(1.0),tf.add(tf.divide(self.a,self.b),tf.divide(self.c,(tf.square(self.f))))))
+    def dag_x(self):
+         return (tf.add(tf.constant(1.0), tf.add(tf.divide(self.a, self.b), tf.divide(self.c, (tf.square(self.f))))))
 
-    def s(self):
-        return tf.divide(tf.subtract(self.b,self.a) , tf.subtract(self.d,self.c))
+    def dag_s(self):
+        return tf.divide(tf.subtract(self.b, self.a) , tf.subtract(self.d, self.c))
 
-    def r(self):
-        return tf.divide(1.0 , (tf.divide(1.0,self.a)+tf.divide(1.0,self.b)+tf.divide(1.0,self.c)+tf.divide(1.0,self.d)))
+    def dag_r(self):
+        return tf.divide(1.0 , (tf.divide(1.0, self.a)+tf.divide(1.0, self.b)+tf.divide(1.0, self.c)+tf.divide(1.0, self.d)))
 
-    def y(self):
-        return tf.multiply( self.a, self.b)*tf.divide(1.0,self.c)*tf.multiply(self.f,tf.divide(self.f,2))
+    def dag_y(self):
+        return tf.multiply( self.a, self.b)*tf.divide(1.0, self.c)*tf.multiply(self.f, tf.divide(self.f, 2))
 
     def test(self):
-        if run(self.x()).item() != 1.4804635047912598:
-            print("Tensors - x is bad: ",run(self.x()).item())
-        if run(self.s()).item() != 13.555558204650879:
-            print("Tensors - s is bad: ",run(self.s()).item())
-        if run(self.r()).item() != 0.2535712718963623:
-            print("Tensors - r is bad: ",run(self.r()).item())
-        if run(self.y()).item() != 715.676513671875:
-            print("Tensors - y s bad: ",run(self.y()).item())
+        if run(self.dag_x()).item() != 1.4804635047912598:
+            print("Tensors - x is bad: ", run(self.dag_x()).item())
+        if run(self.dag_s()).item() != 13.555558204650879:
+            print("Tensors - s is bad: ", run(self.dag_s()).item())
+        if run(self.dag_r()).item() != 0.2535712718963623:
+            print("Tensors - r is bad: ", run(self.dag_r()).item())
+        if run(self.dag_y()).item() != 715.676513671875:
+            print("Tensors - y s bad: ", run(self.dag_y()).item())
       
 
 def problem1():
-    tensor_x = tf.constant(list(range(100,110)))
+    tensor_x = tf.constant(list(range(100, 110)))
     tensor_y = tf.constant([34, 28, 45, 67, 89, 93, 24, 49, 11, 7])
-    tensor_sum = tf.add(tensor_x,tensor_y)
+    tensor_sum = tf.add(tensor_x, tensor_y)
     print("Problem #1")
-    print("\tLazy",tensor_sum)
-    print("\tEager",run(tensor_sum))
+    print("\tLazy", tensor_sum)
+    print("\tEager", run(tensor_sum))
 
 def problem2():
-    x = tf.constant([[1,2,3,4],[5,6,7,8]])
-    t = tf.stack([x,x,x,x])
-    print("Problem #2",t.shape)
+    x = tf.constant([[1, 2, 3, 4], [5, 6, 7, 8]])
+    t = tf.stack([x, x, x, x])
+    print("Problem #2", t.shape)
 
 def problem3():
-    x = tf.constant([[1,2,3,4],[5,6,7,8]])
+    x = tf.constant([[1, 2, 3, 4], [5, 6, 7, 8]])
     t = tf.stack([x])
-    print("Problem #3",t.shape)
+    print("Problem #3", t.shape)
 
 def problem4():
-    x = tf.constant([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
-    t = tf.reshape(x,[6,2])
+    x = tf.constant([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+    t = tf.reshape(x, [6, 2])
     print("Problem #4")
-    print("\t",t.shape)
-    print("\t",run(t))
+    print("\t", t.shape)
+    print("\t", run(t))
 
 def run(tensor):
     with tf.Session() as sess:
@@ -65,12 +65,12 @@ def problem5():
     tensors = Tensors()
     tensors.test()
     print("Problem #5")
-    print("\tx = ",run(tensors.x()))
-    print("\ts = ",run(tensors.s()))
-    print("\tr = ",run(tensors.r()))
-    print("\ty = ",run(tensors.y()))
+    print("\tx = ", run(tensors.dag_x()))
+    print("\ts = ", run(tensors.dag_s()))
+    print("\tr = ", run(tensors.dag_r()))
+    print("\ty = ", run(tensors.dag_y()))
 
-def graphit(name,tensor):
+def graphit(name, tensor):
     with tf.Session() as sess:
         with tf.summary.FileWriter(name, sess.graph) as writer:
              sess.run([tensor])
@@ -78,28 +78,28 @@ def graphit(name,tensor):
 def problem6():
       
     print("Problem #6 - graphs created")
-    graphit("graphs/x",Tensors().x())
-    graphit("graphs/s",Tensors().s())
-    graphit("graphs/r",Tensors().r())
-    graphit("graphs/y",Tensors().y())
+    graphit("graphs/x", Tensors().dag_x())
+    graphit("graphs/s", Tensors().dag_s())
+    graphit("graphs/r", Tensors().dag_r())
+    graphit("graphs/y", Tensors().dag_y())
 
-def is_associative(a,b,c):
-    x = tf.multiply(a,tf.add(b,c))
-    y = tf.add(tf.multiply(a,b),tf.multiply(a,c))
+def is_associative(matrix_a, matrix_b, matrix_c):
+    x = tf.multiply(matrix_a, tf.add(matrix_b, matrix_c))
+    y = tf.add(tf.multiply(matrix_a, matrix_b), tf.multiply(matrix_a, matrix_c))
     return (run(x) == run(y)).all()
 
-def is_distributive(a,b,c):
-    x = tf.multiply(tf.multiply(a,b) ,c)
-    y = tf.multiply(a,tf.multiply(b,c))
+def is_distributive(matrix_a, matrix_b, matrix_c):
+    x = tf.multiply(tf.multiply(matrix_a, matrix_b) , matrix_c)
+    y = tf.multiply(matrix_a, tf.multiply(matrix_b, matrix_c))
     return (run(x) == run(y)).all()
 
 def problem7():
-    a = tf.constant([[4,-2,1],[6,8,-5],[7,9,10]])
-    b = tf.constant([[6,9,-4],[7,5,3],[-8,2,1]])
-    c = tf.constant([[-4,-5,2],[10,6,1],[3,-9,8]])
-    print("Problem #7 - graphs created")
-    print("\tSatisfies the associative property",is_associative(a,b,c))
-    print("\tSatisfies the distributive property",is_distributive(a,b,c))
+    matrix_a = tf.constant([[4, -2, 1], [6, 8, -5], [7, 9, 10]])
+    matrix_b = tf.constant([[6, 9, -4], [7, 5, 3], [-8, 2, 1]])
+    matrix_c = tf.constant([[-4, -5, 2], [10, 6, 1], [3, -9, 8]])
+    print("Problem #7")
+    print("\tSatisfies the associative property", is_associative(matrix_a, matrix_b, matrix_c))
+    print("\tSatisfies the distributive property", is_distributive(matrix_a, matrix_b, matrix_c))
 
 def main():
     problem1()
