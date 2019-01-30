1031a2287f1683c65da92567ea91ad636f17b6f8
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-01-30 21:39:34 +0000

    cleanup

--- lesson3/lesson3.py
+++ lesson3/lesson3.py
@@ -9,37 +9,40 @@ def run(tensor):
 def y_equal_x(x):
     return x
 
-def compute(inputs,weights,bias,activation_fn):
-    layer = tf.matmul(inputs,weights)+bias
-    return activation_fn(layer),layer
+def compute(inputs, weights, bias, activation_fn):
+    layer = tf.matmul(inputs, weights)+bias
+    return activation_fn(layer), layer
 
+##### P R O B L E M 1 ##########
 
 def problem1():
-    inputs = tf.constant([[100,150]])
-    weights = tf.constant([[5,10,15,20],[25,30,35,40]])
-    bias = tf.constant([[17,19,21,23]])
-    activation,l1 = compute(inputs,weights,bias,y_equal_x)
+    inputs = tf.constant([[100, 150]])
+    weights = tf.constant([[5, 10, 15, 20], [25, 30, 35, 40]])
+    bias = tf.constant([[17, 19, 21, 23]])
+    activation, l1 = compute(inputs, weights, bias, y_equal_x)
 
-    weights = tf.constant([[30,35],[40,45],[70,75],[80,85]])
-    bias = tf.constant([[35,36]])
-    activation,l2 = compute(activation,weights,bias,y_equal_x)
+    weights = tf.constant([[30, 35], [40, 45], [70, 75], [80, 85]])
+    bias = tf.constant([[35, 36]])
+    _, l2 = compute(activation, weights, bias, y_equal_x)
     return run(l2)
 
+##### P R O B L E M 2 ##########
+
 def problem2():
-    c = [([0.,0.],0),([1.,0.],1),([0.,1.],1),([1.,1.],0)]
+    c = [([0., 0.], 0), ([1., 0.], 1), ([0., 1.], 1), ([1., 1.], 0)]
     inputs = tf.constant([a[0] for a in c])
-    weights = tf.constant([[-4.,-6.,-5.],[3.,6.,4.]])
-    bias = tf.constant([[-2.,3.,-2.]])
-    activation,l = compute(inputs,weights,bias,tf.sigmoid)
+    weights = tf.constant([[-4., -6., -5.], [3., 6., 4.]])
+    bias = tf.constant([[-2., 3., -2.]])
+    activation, _ = compute(inputs, weights, bias, tf.sigmoid)
   
-    weights = tf.constant([[5.],[-9.],[7.]])
+    weights = tf.constant([[5.], [-9.], [7.]])
     bias = tf.constant([4.])
-    activation,l = compute(activation,weights,bias,tf.sigmoid)
+    activation, _ = compute(activation, weights, bias, tf.sigmoid)
     results = run(activation)
     table = []
     x = 0
     for row in c:
-      r = [row[0][0],row[0][1],row[1],results[x][0],(results[x][0]-row[1])**2]
+      r = [row[0][0], row[0][1], row[1], results[x][0], (results[x][0]-row[1])**2]
       table.append(r)
       x = x + 1
     return table
