e9220e88f611644be0b5de74733af4a8d40f30ef
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-02-06 06:36:29 +0000

    internal examples work

--- lesson4/lesson4.py
+++ lesson4/lesson4.py
@@ -1,50 +1,73 @@
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import numpy as np
-RANDOM_SEED = 42
-n_input = 3
-n_hidden = 5
-n_output = 1
-
-LR = tf.placeholder(tf.float32)
-W1 = tf.Variable(tf.random_uniform([n_input,n_hidden], -1.0,1.0))
-W2 = tf.Variable(tf.random_uniform([n_hidden,n_output], -1.0,1.0))
-B1 = tf.Variable(tf.zeros(n_hidden),name='Bias1')
-B2 = tf.Variable(tf.zeros(n_output),name='Bias2')
-
-f = open("Admissions.csv")
-f.readline()
-dataset = np.loadtxt(fname = f, delimiter = ',')
-features = dataset[:,1:] # antecedents
-labels = dataset[:,:1]  # consequent
-
-# a "1" in col 0 means not admitted
-# a "1" in col 1 means admitted
-one_hot = np.zeros(shape=(len(labels),2))
-for i in range(0,len(labels)):
-    one_hot[i,int(labels[i])] = 1
-
-train_feats,tet_feats,train_lab,test_lab = train_test_split(features, one_hot,test_size =0.33 ,random_state=RANDOM_SEED)
-
-
-def run(tensor):
-    with tf.Session() as sess:
-        return(sess.run(tensor))
-
-def compute(inputs, weights, bias, activation_fn):
-    layer = tf.matmul(inputs, weights)+bias
-    return activation_fn(layer), layer
+from sklearn import datasets
+import sys
 
 def problem1():
-    print(W1)
-    print(W2)
-    print(B1)
-    print(B2)
-    print(dataset[:5,:])
-    print(features[:5,:])
-    print(labels[:5,:])
-    print(one_hot[:5,:])
-    print(train_feats.shape)
+    RANDOM_SEED = 42
+    n_hidden = 10
+
+    #f = open("Admissions.csv")
+    #f.readline()
+    #dataset = np.loadtxt(fname = f, delimiter = ',')
+    #features = dataset[:,1:] # antecedents
+    #labels = dataset[:,:1]  # consequent
+    iris = datasets.load_iris()
+    features = iris["data"]
+    labels = iris["target"]
+
+    # a "1" in col 0 means not admitted
+    # a "1" in col 1 means admitted
+    one_hot = np.zeros(shape=(len(labels),3))
+    for i in range(0,len(labels)):
+        one_hot[i,int(labels[i])] = 1
+    train_feats,test_feats,train_lab,test_lab = train_test_split(
+            features, one_hot,test_size = 0.30 ,random_state=RANDOM_SEED)
+    feat_shape = train_feats.shape[1]
+    hidden_nodes = 3
+    out_shape = train_lab.shape[1]
+
+    inputs = tf.placeholder("float", shape=[None, feat_shape])
+    outputs = tf.placeholder("float", shape=[None, out_shape])
+    Z = tf.placeholder("float")
+
+    W1 = tf.get_variable(name="W1",
+            shape=[feat_shape, n_hidden],
+            initializer=tf.contrib.layers.xavier_initializer())
+    B1 = tf.get_variable(name="b1",
+            shape=[n_hidden],
+            initializer=tf.constant_initializer(0.0))
+    H1 = tf.matmul(inputs, W1) + B1
+    #H1 = tf.sigmoid(H1)
+    H1 = tf.nn.relu(H1)
+
+    W2 = tf.get_variable(name="W2",
+            shape=[n_hidden, out_shape],
+            initializer=tf.contrib.layers.xavier_initializer())
+    B2 = tf.get_variable(name="b2",
+            shape=[out_shape],
+            initializer=tf.constant_initializer(0.0))
+    #pred_tensor = tf.sigmoid(tf.matmul(H1, W2) + B2)
+    #pred_tensor = tf.nn.relu(tf.matmul(H1, W2) + B2)
+    pred_tensor = tf.matmul(H1, W2) + B2
+
+    predict = tf.argmax(pred_tensor, axis=1)
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=outputs, logits=pred_tensor))
+    #cost = tf.reduce_mean(-outputs*tf.log(pred_tensor) - (1-outputs)*tf.log(1-pred_tensor))
+    updates = tf.train.GradientDescentOptimizer(Z).minimize(cost)
+    with tf.Session() as sess:
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        for epoch in range(100):
+            # Train with each example
+            for i in range(len(train_feats)):
+                op,cst = sess.run([updates,cost],
+                        feed_dict={inputs: train_feats[i: i + 1], outputs: train_lab[i: i + 1],Z:0.01})
+            test_accuracy = np.mean(np.argmax(test_lab, axis=1) == sess.run(predict, feed_dict={inputs: test_feats, outputs: test_lab}))
+            if (epoch % 10) == 0:
+                print("Epoch: %d, acc: %.2f, cost: %.5f" % (epoch, test_accuracy, cst))
 
 def main():
     problem1()
