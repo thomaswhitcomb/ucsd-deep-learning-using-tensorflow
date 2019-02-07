257024377e3df69bf814d7e8e4502a38d1ad22f4
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-02-07 22:03:01 +0000

    understand accuracy

--- lesson4/lesson4.py
+++ lesson4/lesson4.py
@@ -13,15 +13,12 @@ def cost3(target,compValue):
     return tf.reduce_mean(tf.pow(target - compValue,2))
 
 
-def compute(features,labels,p_hidden,p_cost_fn=cost1,p_activation_fn=tf.nn.relu,p_epochs=100,p_learning_rate=0.1,p_test_size=0.0,p_initializer=tf.initializers.random_uniform):
+def train(features,labels,p_hidden,p_cost_fn=cost1,p_activation_fn=tf.nn.relu,p_epochs=100,p_learning_rate=0.1,p_test_size=0.0,p_initializer=tf.initializers.random_uniform):
     RANDOM_SEED = 42
     tf.set_random_seed(RANDOM_SEED)
 
     input_shape = features.shape[1]
     out_shape = labels.shape[1]
-    #print("out shape: ",out_shape)
-    #print("input shape: ",input_shape)
-    #print("hidden: ",p_hidden)
 
     Inputs = tf.placeholder(tf.float32, shape=[None, input_shape])
     Outputs = tf.placeholder(tf.float32, shape=[None, out_shape])
@@ -46,10 +43,9 @@ def compute(features,labels,p_hidden,p_cost_fn=cost1,p_activation_fn=tf.nn.relu,
     O1wo = tf.matmul(H1, W2) + B2
     O1 = p_activation_fn(O1wo)
     #O1 = tf.matmul(H1, W2) + B2
-
-    n_axis = 0
+    n_axis = 1
     predict = tf.argmax(O1, axis=n_axis)
-    correct_prediction = tf.equal(tf.argmax(O1,1), tf.argmax(Y,1))
+    correct_prediction = tf.equal(tf.argmax(O1,axis=n_axis), tf.argmax(Y,axis=n_axis))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     cost = p_cost_fn(Outputs,O1)
     updates = tf.train.GradientDescentOptimizer(Learning_rate).minimize(cost)
@@ -62,6 +58,10 @@ def compute(features,labels,p_hidden,p_cost_fn=cost1,p_activation_fn=tf.nn.relu,
     #print("train_labels shape:",train_labels.shape)
     #print("train_features:",train_features[:5,:])
     #print("train_labels:",train_labels[:5,:])
+    #print("test_labels:",test_labels.shape,test_labels[:5,:])
+    #print("np.argmax(test_labels,axis=1):",np.argmax(test_labels))
+    #print("test_labels[np.argmax(test_labels,axis=1)]:",test_labels[np.argmax(test_labels)])
+    #print("np.mean(np.argmax(test_labels,axis=1)):",np.mean(np.argmax(test_labels)))
     with tf.Session() as sess:
         init = tf.global_variables_initializer()
         sess.run(init)
@@ -69,14 +69,19 @@ def compute(features,labels,p_hidden,p_cost_fn=cost1,p_activation_fn=tf.nn.relu,
             # Train with each example
             for i in range(len(train_features)):
                 op,cst = sess.run([updates,cost], feed_dict={Inputs: train_features[i: i + 1], Outputs: train_labels[i: i + 1],Learning_rate:p_learning_rate})
+            test_accuracy = np.mean(np.argmax(test_labels, axis=n_axis) == sess.run(predict, feed_dict={Inputs: test_features, Outputs: test_labels}))
+            test_accuracy1 = sess.run(accuracy,feed_dict={Inputs:test_features,Y:test_labels})
             if (epoch % (p_epochs/10)) == 0:
-                if len(test_features) > 0:
-                    test_accuracy = np.mean(np.argmax(test_labels, axis=n_axis) == sess.run(predict, feed_dict={Inputs: test_features, Outputs: test_labels}))
-                    test_accuracy1 = sess.run(accuracy,feed_dict={Inputs:test_features,Y:test_labels})
-                else:
-                    test_accuracy = 0.0
-                    test_accuracy1 = 0.0
-
+                x1 = np.argmax(test_labels, axis=n_axis)
+                x2 = sess.run(predict, feed_dict={Inputs: test_features, Outputs: test_labels})
+                x3 = np.equal(x1,x2)
+                x4 = np.mean(x3)
+                print("observed labels:", test_labels)
+                print("generated labels:", x2)
+                print("x1:", x1)
+                print("x2:", x2)
+                print("x3:", x3)
+                print("x4:", x4)
                 print("Epoch: %d, acc: %.5f,acc: %.5f, cost: %.5f" % (epoch, test_accuracy,test_accuracy1, cst))
 
 def verify1():
@@ -86,7 +91,7 @@ def verify1():
     features = dataset[:,1:] # antecedents
     labels = dataset[:,:1]  # consequent
 
-    compute(
+    train(
             features,
             labels,
             3,
@@ -106,7 +111,7 @@ def verify2():
     for i in range(0,len(labels)):
         one_hot[i,labels[i]] = 1
     print("one hot: ",one_hot[:5,:])
-    compute(
+    train(
             features,
             one_hot,
             10,
@@ -125,7 +130,7 @@ def problem1():
     features_scaled = features/features.max(axis=0)
     labels = dataset[:,:1]  # consequent
 
-    compute(
+    train(
             features_scaled,
             labels,
             10,
@@ -144,15 +149,15 @@ def problem2():
     labels = dataset[:,4:]  # consequent
     features_scaled = features/features.max(axis=0)
     labels_scaled = labels/labels.max()
-    compute(
+    train(
             features_scaled,
-            labels,
+            labels_scaled,
             10,
             p_epochs=100,
             p_cost_fn=cost3,
             #p_activation_fn=tf.sigmoid,
             p_activation_fn=tf.nn.relu,
-            p_learning_rate=0.01,
+            p_learning_rate=0.001,
             #p_initializer=tf.contrib.layers.xavier_initializer,
             p_initializer=tf.initializers.random_uniform,
             p_test_size=0.33)    
