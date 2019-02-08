7d2409b18828ff47327ec6c8d9abfa107725225a
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-02-08 06:00:59 +0000

    got rmse

--- lesson4/lesson4.py
+++ lesson4/lesson4.py
@@ -45,6 +45,7 @@ def train(features,labels,p_hidden,p_cost_fn=cost1,p_activation_fn=tf.nn.relu,p_
     #O1 = tf.matmul(H1, W2) + B2
     n_axis = 1
     predict = tf.argmax(O1, axis=n_axis)
+    predict2 = O1
     correct_prediction = tf.equal(tf.argmax(O1,axis=n_axis), tf.argmax(Y,axis=n_axis))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     cost = p_cost_fn(Outputs,O1)
@@ -72,17 +73,63 @@ def train(features,labels,p_hidden,p_cost_fn=cost1,p_activation_fn=tf.nn.relu,p_
             test_accuracy = np.mean(np.argmax(test_labels, axis=n_axis) == sess.run(predict, feed_dict={Inputs: test_features, Outputs: test_labels}))
             test_accuracy1 = sess.run(accuracy,feed_dict={Inputs:test_features,Y:test_labels})
             if (epoch % (p_epochs/10)) == 0:
-                x1 = np.argmax(test_labels, axis=n_axis)
-                x2 = sess.run(predict, feed_dict={Inputs: test_features, Outputs: test_labels})
-                x3 = np.equal(x1,x2)
-                x4 = np.mean(x3)
-                print("observed labels:", test_labels)
-                print("generated labels:", x2)
-                print("x1:", x1)
-                print("x2:", x2)
-                print("x3:", x3)
-                print("x4:", x4)
                 print("Epoch: %d, acc: %.5f,acc: %.5f, cost: %.5f" % (epoch, test_accuracy,test_accuracy1, cst))
+
+def train2(features,labels,p_hidden,p_cost_fn=cost1,p_activation_fn=tf.nn.relu,p_epochs=100,p_learning_rate=0.1,p_test_size=0.0,p_initializer=tf.initializers.random_uniform):
+    RANDOM_SEED = 42
+    tf.set_random_seed(RANDOM_SEED)
+
+    input_shape = features.shape[1]
+    out_shape = labels.shape[1]
+
+    Inputs = tf.placeholder(tf.float32, shape=[None, input_shape])
+    Outputs = tf.placeholder(tf.float32, shape=[None, out_shape])
+    Y = tf.placeholder(tf.float32, shape=[None, out_shape])
+    Learning_rate = tf.placeholder(tf.float32)
+
+    W1 = tf.get_variable(name="W1",
+            shape=[input_shape, p_hidden],
+            initializer=p_initializer())
+    B1 = tf.get_variable(name="B1",
+            shape=[p_hidden],
+            initializer=tf.constant_initializer(0.0))
+    H1 = p_activation_fn(tf.matmul(Inputs, W1) + B1)
+
+    W2 = tf.get_variable(name="W2",
+            shape=[p_hidden, out_shape],
+            initializer=p_initializer())
+    B2 = tf.get_variable(name="B2",
+            shape=[out_shape],
+            initializer=tf.constant_initializer(0.0))
+
+    O1wo = tf.matmul(H1, W2) + B2
+    O1 = p_activation_fn(O1wo)
+    #O1 = tf.matmul(H1, W2) + B2
+    n_axis = 1
+    predict = tf.argmax(O1, axis=n_axis)
+    predict2 = O1
+    correct_prediction = tf.equal(tf.argmax(O1,axis=n_axis), tf.argmax(Y,axis=n_axis))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    cost = p_cost_fn(Outputs,O1)
+    updates = tf.train.GradientDescentOptimizer(Learning_rate).minimize(cost)
+
+    train_features,test_features,train_labels,test_labels = train_test_split(
+            features, labels,test_size=p_test_size ,random_state=RANDOM_SEED)
+    with tf.Session() as sess:
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        for epoch in range(p_epochs):
+            # Train with each example
+            for i in range(len(train_features)):
+                op,cst = sess.run([updates,cost], feed_dict={Inputs: train_features[i: i + 1], Outputs: train_labels[i: i + 1],Learning_rate:p_learning_rate})
+            #test_accuracy = np.mean(np.argmax(test_labels, axis=n_axis) == sess.run(predict, feed_dict={Inputs: test_features, Outputs: test_labels}))
+
+            #test_accuracy1 = sess.run(accuracy,feed_dict={Inputs:test_features,Y:test_labels})
+            if (epoch % (p_epochs/10)) == 0:
+                xx1 = np.stack(test_labels,axis=0)
+                xx2 = np.stack(sess.run(O1,feed_dict={Inputs:test_features,Y:test_labels}),axis=0)
+                test_accuracy = sess.run(tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(xx1,xx2)))))
+                print("Epoch: %d, acc: %.5f, cost: %.5f" % (epoch, test_accuracy, cst))
 
 def verify1():
     f = open("Xor.csv")
@@ -125,20 +172,25 @@ def verify2():
 def problem1():
     f = open("Admissions.csv")
     f.readline()
-    dataset = np.loadtxt(fname = f, delimiter = ',')
+    dataset = np.genfromtxt(fname = f, delimiter = ',')
     features = dataset[:,1:] # antecedents
     features_scaled = features/features.max(axis=0)
     labels = dataset[:,:1]  # consequent
+    one_hot = np.zeros(shape=(len(labels),2))
+    for i in range(0,len(labels)):
+        one_hot[i,int(labels[i])] = 1
 
     train(
             features_scaled,
-            labels,
+            one_hot,
             10,
-            p_epochs=100,
+            p_epochs=1000,
             p_cost_fn=cost1,
             p_activation_fn=tf.sigmoid,
-            p_learning_rate=0.1,
-            p_initializer=tf.initializers.random_uniform,
+            #p_activation_fn=tf.nn.relu,
+            p_learning_rate=0.01,
+            p_initializer=tf.contrib.layers.xavier_initializer,
+            #p_initializer=tf.initializers.random_uniform,
             p_test_size=0.30)    
 
 def problem2():
@@ -146,18 +198,21 @@ def problem2():
     f.readline()
     dataset = np.genfromtxt(fname = f, delimiter = ',')
     features = dataset[:,1:4] # antecedents
+    print("features.shape:",features.shape)
     labels = dataset[:,4:]  # consequent
+    print("labels.shape:",labels.shape)
     features_scaled = features/features.max(axis=0)
+    print("features_scaled.shape:",features_scaled.shape)
     labels_scaled = labels/labels.max()
-    train(
+    print("labels_scaled.shape:",labels_scaled.shape)
+    train2(
             features_scaled,
             labels_scaled,
-            10,
-            p_epochs=100,
+            5,
+            p_epochs=5000,
             p_cost_fn=cost3,
-            #p_activation_fn=tf.sigmoid,
             p_activation_fn=tf.nn.relu,
-            p_learning_rate=0.001,
+            p_learning_rate=0.01,
             #p_initializer=tf.contrib.layers.xavier_initializer,
             p_initializer=tf.initializers.random_uniform,
             p_test_size=0.33)    
