d17abd7c15465a099b74247d483779eaeebda90f
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-02-14 19:32:10 +0000

    added asserts

--- lesson5/lesson5.py
+++ lesson5/lesson5.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 from sklearn import linear_model
 class Problem1Base():
@@ -14,15 +14,13 @@ class Problem1Base():
         plt.plot(self.x_point, self.y_point,'o')
         plt.savefig("lesson5.png")
 
-
-
 class Problem1SK(Problem1Base):
     def __init__(self):
-        pass
+        self.RANDOM_SEED = 42
+        np.random.seed(self.RANDOM_SEED) 
 
     def compute_regression(self):
         linreg = linear_model.LinearRegression()
-        #self.x_point = np.reshape(self.x_point,(len(self.x_point),1))
         self.x_point = np.reshape(self.x_point,(len(self.x_point),1))
         linreg.fit(self.x_point,self.y_point,self.y_point)
         return linreg.coef_,linreg.intercept_
@@ -35,12 +33,14 @@ class Problem1TF(Problem1Base):
         np.random.seed(self.RANDOM_SEED) 
 
     def build(self):
-        with self.graph.as_default():
-            self._slope = tf.Variable(tf.random_uniform([1],-1.0,1.0))
-            self._intercept = tf.Variable(tf.zeros([1]))
-            _response = self._slope*self.x_point + self._intercept
-            self._cost = tf.reduce_mean(tf.square(_response - self.y_point))
-            self._optimizer = tf.train.GradientDescentOptimizer(0.0001).minimize(self._cost)
+        self.X = tf.placeholder(tf.float32)
+        self.Y = tf.placeholder(tf.float32)
+        self.LR = tf.placeholder(tf.float32)
+        self._slope = tf.Variable(tf.random_uniform([1],-1.0,1.0))
+        self._intercept = tf.Variable(tf.zeros([1]))
+        self._response = self.slope*self.X + self.intercept
+        self._cost = tf.reduce_mean(tf.square(self._response - self.Y))
+        self._optimizer = tf.train.GradientDescentOptimizer(self.LR).minimize(self.cost)
 
     @property
     def cost(self):
@@ -55,26 +55,43 @@ class Problem1TF(Problem1Base):
     def intercept(self):
         return self._intercept
 
-    def compute_regression(self):
-        with tf.Session(graph=self.graph) as session:
-            init = tf.global_variables_initializer()
+    def compute_regression(self,epochs,learning_rate):
+        init = tf.global_variables_initializer()
+        with tf.Session() as session:
             session.run(init)
-            for epoch in range(50000):
-                session.run(self.optimizer)
-                if (epoch % 500) == 0:
-                    c = session.run(self._cost)
-                    print(epoch,c,session.run(self._slope),session.run(self._intercept))
-            print("Slope = ",session.run(self.slope))
-            print("Intercept = ",session.run(self.intercept))
+            for epoch in range(epochs):
+                session.run(self._optimizer,feed_dict = {self.LR: learning_rate,self.X : self.x_point, self.Y : self.y_point})
+                if (epoch % (epochs/20)) == 0:
+                    c = session.run(self.cost, feed_dict = {self.LR: learning_rate,self.X : self.x_point, self.Y : self.y_point})
+                    print("Epoch:", epoch , "Cost:", c, "Slope:", session.run(self.slope), "Intercept:", session.run(self.intercept))
+            c = session.run(self.cost, feed_dict = {self.LR: learning_rate,self.X : self.x_point, self.Y : self.y_point})
+            print("Epoch:", epoch , "Cost:", c, "Slope:", session.run(self.slope), "Intercept:", session.run(self.intercept))
+            return c,session.run(self.slope),session.run(self.intercept)
 
 def main():
     problem1 = Problem1SK()
     problem1.create_dataset()
-    print(problem1.compute_regression())
+    slope,intercept = problem1.compute_regression()
+    print(slope,intercept)
+    assert ("%.8f" % slope[0]) == "3.54942311"
+    assert ("%.15f" % intercept) == "14.841075232789862"
+
     problem1 = Problem1TF()
     problem1.create_dataset()
     problem1.build()
-    problem1.compute_regression()
+    cost,slope,intercept = problem1.compute_regression(50000,0.0001)
+    print(cost,slope,intercept)
+    assert ("%.5f" % cost) == "11.36642"
+    assert ("%.7f" % slope[0]) == "3.5775077"
+    assert ("%.6f" % intercept[0]) == "14.184714"
 
+    problem1 = Problem1TF()
+    problem1.create_dataset()
+    problem1.build()
+    cost,slope,intercept = problem1.compute_regression(5000,0.001)
+    print(cost,slope,intercept)
+    assert ("%.6f" % cost) == "11.365601"
+    assert ("%.7f" % slope[0]) == "3.5774026"
+    assert ("%.6f" % intercept[0]) == "14.186133"
 if __name__ == "__main__":
     main()
