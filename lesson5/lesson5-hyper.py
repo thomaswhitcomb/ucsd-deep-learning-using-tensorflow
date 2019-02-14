f282b5783f7bdea99f243994e10f01fb9fedece2
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-02-14 06:45:21 +0000

    dual solutions working

--- lesson5/lesson5-hyper.py
+++ lesson5/lesson5-hyper.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 from sklearn import linear_model
 class Problem1Base():
@@ -14,15 +14,12 @@ class Problem1Base():
         plt.plot(self.x_point, self.y_point,'o')
         plt.savefig("lesson5.png")
 
-
-
 class Problem1SK(Problem1Base):
     def __init__(self):
         pass
 
     def compute_regression(self):
         linreg = linear_model.LinearRegression()
-        #self.x_point = np.reshape(self.x_point,(len(self.x_point),1))
         self.x_point = np.reshape(self.x_point,(len(self.x_point),1))
         linreg.fit(self.x_point,self.y_point,self.y_point)
         return linreg.coef_,linreg.intercept_
@@ -35,12 +32,14 @@ class Problem1TF(Problem1Base):
         np.random.seed(self.RANDOM_SEED) 
 
     def build(self):
-        with self.graph.as_default():
-            self._slope = tf.Variable(tf.random_uniform([1],-1.0,1.0))
-            self._intercept = tf.Variable(tf.zeros([1]))
-            _response = self._slope*self.x_point + self._intercept
-            self._cost = tf.reduce_mean(tf.square(_response - self.y_point))
-            self._optimizer = tf.train.GradientDescentOptimizer(0.0001).minimize(self._cost)
+        self.X = tf.placeholder(tf.float32)
+        self.Y = tf.placeholder(tf.float32)
+        self.LR = tf.placeholder(tf.float32)
+        self._slope = tf.Variable(tf.random_uniform([1],-1.0,1.0))
+        self._intercept = tf.Variable(tf.zeros([1]))
+        self._response = self._slope*self.X + self._intercept
+        self._cost = tf.reduce_mean(tf.square(self._response - self.Y))
+        self._optimizer = tf.train.GradientDescentOptimizer(self.LR).minimize(self._cost)
 
     @property
     def cost(self):
@@ -56,25 +55,28 @@ class Problem1TF(Problem1Base):
         return self._intercept
 
     def compute_regression(self):
-        with tf.Session(graph=self.graph) as session:
-            init = tf.global_variables_initializer()
+        init = tf.global_variables_initializer()
+        with tf.Session() as session:
             session.run(init)
             for epoch in range(50000):
-                session.run(self.optimizer)
-                if (epoch % 500) == 0:
-                    c = session.run(self._cost)
-                    print(epoch,c,session.run(self._slope),session.run(self._intercept))
-            print("Slope = ",session.run(self.slope))
-            print("Intercept = ",session.run(self.intercept))
+    #           for (_x,_y) in zip(self.x_point,self.y_point):
+    #               session.run(self._optimizer,feed_dict = {self.LR: 0.0001,self.X : _x, self.Y : _y})
+                session.run(self._optimizer,feed_dict = {self.LR: 0.0001,self.X : self.x_point, self.Y : self.y_point})
+                if (epoch % 500) == 0:    
+                    c = session.run(self._cost, feed_dict = {self.LR: 0.0001,self.X : self.x_point, self.Y : self.y_point})
+                    print("Epoch", epoch , ": cost =", c, "slope =", session.run(self._slope), "intercept =", session.run(self._intercept))
+            print("Slope = ",session.run(self._slope))
+            print("Intercept = ",session.run(self._intercept))
 
 def main():
     problem1 = Problem1SK()
     problem1.create_dataset()
     print(problem1.compute_regression())
+
     problem1 = Problem1TF()
     problem1.create_dataset()
     problem1.build()
-    problem1.compute_regression()
+    print(problem1.compute_regression())
 
 if __name__ == "__main__":
     main()
