b611f6f3de83a5f6cd0414d77d1f9fea4fdfa64a
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-02-17 07:59:21 +0000

    got it

--- lesson4/lesson4.py
+++ lesson4/lesson4.py
@@ -8,6 +8,7 @@ import sys
 
 class Graph:
     def __init__(self):
+        tf.reset_default_graph()
         tf.set_random_seed(self.random_seed())
 
     def cost_calc(self):
@@ -32,32 +33,32 @@ class Graph:
 
         input_shape = self.features.shape[1]
         out_shape = self.labels.shape[1]
-        self.Inputs = tf.placeholder(tf.float32, shape=[None, input_shape])
-        self.Outputs = tf.placeholder(tf.float32, shape=[None, out_shape])
-        self.TestOutputs = tf.placeholder(tf.float32, shape=[None, out_shape])
-        self.Learning_rate = tf.placeholder(tf.float32)
+        self.inputs = tf.placeholder(tf.float32, shape=[None, input_shape])
+        self.output = tf.placeholder(tf.float32, shape=[None, out_shape])
+        self.test_outputss = tf.placeholder(tf.float32, shape=[None, out_shape])
+        self.learning_rate = tf.placeholder(tf.float32)
     
-        self.W1 = tf.get_variable(name="W1",
+        self.w1 = tf.get_variable(name="w1",
                 shape=[input_shape, hidden],
                 initializer=self.variable_initializer()())
-        self.B1 = tf.get_variable(name="B1",
+        self.b1 = tf.get_variable(name="b1",
                 shape=[hidden],
                 initializer=tf.constant_initializer(0.0))
-        self.H1 = self.activation_calc()(tf.matmul(self.Inputs, self.W1) + self.B1)
-        self.W2 = tf.get_variable(name="W2",
+        self.h1 = self.activation_calc()(tf.matmul(self.inputs, self.w1) + self.b1)
+        self.w2 = tf.get_variable(name="w2",
                 shape=[hidden, out_shape],
                 initializer=self.variable_initializer()())
-        self.B2 = tf.get_variable(name="B2",
+        self.b2 = tf.get_variable(name="b2",
                 shape=[out_shape],
                 initializer=tf.constant_initializer(0.0))
 
-        self.O1 = self.activation_calc()(tf.matmul(self.H1, self.W2) + self.B2)
+        self.o1 = self.activation_calc()(tf.matmul(self.h1, self.w2) + self.b2)
 
-        correct_prediction = tf.equal(tf.argmax(self.O1,axis=1), tf.argmax(self.TestOutputs,axis=1))
+        correct_prediction = tf.equal(tf.argmax(self.o1,axis=1), tf.argmax(self.test_outputss,axis=1))
         self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-        self.cost = self.cost_calc()(self.Outputs,self.O1)
-        self.updates = tf.train.GradientDescentOptimizer(self.Learning_rate).minimize(self.cost)
+        self.cost = self.cost_calc()(self.output,self.o1)
+        self.updates = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.cost)
 
     def train(self,tsize,epochs,lr):
         train_features,test_features,train_labels,test_labels = train_test_split(
@@ -68,15 +69,15 @@ class Graph:
             for epoch in range(epochs+1):
                 # Train with each example
                 for i in range(len(train_features)):
-                    op,cst = sess.run([self.updates,self.cost], feed_dict={self.Inputs: train_features[i: i + 1], self.Outputs: train_labels[i: i + 1],self.Learning_rate:lr})
+                    op,cst = sess.run([self.updates,self.cost], feed_dict={self.inputs: train_features[i: i + 1], self.output: train_labels[i: i + 1],self.learning_rate:lr})
                 if (epoch % (epochs/20)) == 0:
                     test_accuracy = self.calc_accuracy(sess,test_features,test_labels)
                     print("Epoch: %d, accuracy: %.5f, cost: %.5f" % (epoch, test_accuracy, cst))
 
-            print("W1: ",sess.run(self.W1))
-            print("B1: ",sess.run(self.B1))
-            print("W2: ",sess.run(self.W2))
-            print("B2: ",sess.run(self.B2))
+            print("W1: ",sess.run(self.w1))
+            print("B1: ",sess.run(self.b1))
+            print("W2: ",sess.run(self.w2))
+            print("B2: ",sess.run(self.b2))
  
             return test_accuracy,cst
 
@@ -90,7 +91,7 @@ class Graph1(Graph):
     def variable_initializer(self):
         return tf.contrib.layers.xavier_initializer
     def calc_accuracy(self,sess,features,labels):
-        return sess.run(self.accuracy,feed_dict={self.Inputs:features,self.TestOutputs:labels})
+        return sess.run(self.accuracy,feed_dict={self.inputs:features,self.test_outputss:labels})
 
 class Graph2(Graph):
     def cost_calc(self):
@@ -106,17 +107,20 @@ class Graph2(Graph):
 
     def calc_accuracy(self,sess,features,labels):
         xx1 = np.stack(labels,axis=0)
-        xx2 = np.stack(sess.run(self.O1,feed_dict={self.Inputs:features,self.TestOutputs:labels}),axis=0)
+        xx2 = np.stack(sess.run(self.o1,feed_dict={self.inputs:features,self.test_outputss:labels}),axis=0)
         return sess.run(tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(xx1,xx2)))))
+
+def scale(t):
+    tMin = t.min(axis=0)
+    tMax = t.max(axis=0)
+    return (t-tMin)/(tMax-tMin)
 
 def problem1(epochs,learning_rate,hidden):
     f = open("Admissions.csv")
     f.readline()
     dataset = np.genfromtxt(fname = f, delimiter = ',')
     features = dataset[:,1:] # antecedents
-    featuresMin = features.min(axis=0)
-    featuresMax = features.max(axis=0)
-    features_scaled = (features-featuresMin)/(featuresMax-featuresMin)
+    features_scaled = scale(features)
     labels = dataset[:,0:1]  # consequent
     one_hot = np.zeros(shape=(len(labels),2))
     for i in range(0,len(labels)):
@@ -131,13 +135,9 @@ def problem2(epochs,learning_rate,hidden):
     f.readline()
     dataset = np.genfromtxt(fname = f, delimiter = ',')
     features = dataset[:,1:4] # antecedents
-    featuresMin = features.min(axis=0)
-    featuresMax = features.max(axis=0)
-    features_scaled = (features-featuresMin)/(featuresMax-featuresMin)
+    features_scaled = scale(features)
     labels = dataset[:,4:]  # consequent
-    labelsMin = labels.min(axis=0)
-    labelsMax = labels.max(axis=0)
-    labels_scaled = (labels-labelsMin)/(labelsMax-labelsMin)
+    labels_scaled = scale(labels)
     g = Graph2()
     g.initialize(features_scaled,labels_scaled,hidden)
     return g.train(0.30,epochs,learning_rate)
