cb13f02e0faf24f729302bb9a6a8789be7378dd0
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-02-18 03:40:33 +0000

    ready for class submission

--- lesson5/lesson5.py
+++ lesson5/lesson5.py
@@ -3,14 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 from sklearn import linear_model
+
+def scale(t):
+    tMin = t.min(axis=0)
+    tMax = t.max(axis=0)
+    return (t-tMin)/(tMax-tMin)
+
 class Problem1Base():
     def __init__(self):
-        pass
+        tf.reset_default_graph()
 
     def create_dataset(self):
         n_samples = 30
         self.x_point = np.linspace(0,20,n_samples)
+
         self.y_point = 3.7 * self.x_point + 14 + 4 * np.random.randn(n_samples)
+
         plt.plot(self.x_point, self.y_point,'o')
         plt.savefig("lesson5.png")
 
@@ -36,61 +44,58 @@ class Problem1TF(Problem1Base):
         self.X = tf.placeholder(tf.float32)
         self.Y = tf.placeholder(tf.float32)
         self.LR = tf.placeholder(tf.float32)
-        self._slope = tf.Variable(tf.random_uniform([1],-1.0,1.0))
-        self._intercept = tf.Variable(tf.zeros([1]))
-        self._response = self.slope*self.X + self.intercept
-        self._cost = tf.reduce_mean(tf.square(self._response - self.Y))
-        self._optimizer = tf.train.GradientDescentOptimizer(self.LR).minimize(self.cost)
-
-    @property
-    def cost(self):
-        return self._cost
-    @property
-    def optimizer(self):
-        return self._optimizer
-    @property
-    def slope(self):
-        return self._slope
-    @property
-    def intercept(self):
-        return self._intercept
+        self.slope = tf.Variable(tf.random_uniform([1],-1.0,1.0))
+        self.intercept = tf.Variable(tf.zeros([1]))
+        self.response = self.slope*self.X + self.intercept
+        self.cost = tf.reduce_mean(tf.square(self.response - self.Y))
+        self.optimizer = tf.train.GradientDescentOptimizer(self.LR).minimize(self.cost)
 
     def compute_regression(self,epochs,learning_rate):
         init = tf.global_variables_initializer()
         with tf.Session() as session:
             session.run(init)
             for epoch in range(epochs):
-                session.run(self._optimizer,feed_dict = {self.LR: learning_rate,self.X : self.x_point, self.Y : self.y_point})
+                session.run(
+                        self.optimizer,feed_dict = {
+                            self.LR: learning_rate,
+                            self.X : self.x_point, 
+                            self.Y : self.y_point})
                 if (epoch % (epochs/20)) == 0:
-                    c = session.run(self.cost, feed_dict = {self.LR: learning_rate,self.X : self.x_point, self.Y : self.y_point})
-                    print("Epoch:", epoch , "Cost:", c, "Slope:", session.run(self.slope), "Intercept:", session.run(self.intercept))
-            c = session.run(self.cost, feed_dict = {self.LR: learning_rate,self.X : self.x_point, self.Y : self.y_point})
-            print("Epoch:", epoch , "Cost:", c, "Slope:", session.run(self.slope), "Intercept:", session.run(self.intercept))
+                    c = session.run(
+                            self.cost, feed_dict = {
+                                self.LR: learning_rate,
+                                self.X : self.x_point, 
+                                self.Y : self.y_point})
+                    print("Epoch:", epoch , "Cost:", c, "Slope:", 
+                            session.run(self.slope), 
+                            "Intercept:", 
+                            session.run(self.intercept))
+            c = session.run(self.cost, feed_dict = {
+                self.LR: learning_rate,
+                self.X : self.x_point, 
+                self.Y : self.y_point})
+            print("Epoch:", epoch , "Cost:", c, "Slope:", 
+                    session.run(self.slope), 
+                    "Intercept:", 
+                    session.run(self.intercept))
             return c,session.run(self.slope),session.run(self.intercept)
 
 class Problem2Base():
     def __init__(self):
-        pass
+        tf.reset_default_graph()
 
     def create_dataset(self):
         f = open("00 kc_house_data.csv")
         f.readline()
         dataset = np.genfromtxt(fname = f, delimiter = ',',usecols=(2,3,5))
-        #print(dataset[:5,:])
         self.predictors = dataset[:,1:3] 
-        #print(self.predictors[:5,:])
         predictorsMin = self.predictors.min(axis=0)
         predictorsMax = self.predictors.max(axis=0)
-        self.predictors_scaled = (self.predictors-predictorsMin)/(predictorsMax-predictorsMin)
-        #print("predictors scaled",self.predictors_scaled[:5,:])
-        #print("predictors scaled col1",self.predictors_scaled[:5,0:1])
-        #print("predictors scaled col2",self.predictors_scaled[:5,1:2])
+        self.predictors_scaled = scale(self.predictors)
         self.response = dataset[:,0:1]
-        #print("response",self.response[:5,:])
         responseMin = self.response.min(axis=0)
         responseMax = self.response.max(axis=0)
-        self.response_scaled = (self.response-responseMin)/(responseMax-responseMin)
-        #print("response scaled",self.response_scaled[:5,:])
+        self.response_scaled = scale(self.response)
 
 class Problem2SK(Problem2Base):
     def __init__(self):
@@ -127,12 +132,38 @@ class Problem2TF(Problem2Base):
         with tf.Session() as session:
             session.run(init)
             for epoch in range(epochs):
-                session.run(self.optimizer,feed_dict = {self.LR: learning_rate,self.X1:self.predictors_scaled[:,0:1],self.X2:self.predictors_scaled[:,1:2],self.Y:self.response_scaled})
+                session.run(
+                        self.optimizer,feed_dict = {
+                            self.LR: learning_rate,
+                            self.X1:self.predictors_scaled[:,0:1],
+                            self.X2:self.predictors_scaled[:,1:2],
+                            self.Y:self.response_scaled})
                 if not epoch % 100000:
-                    c = session.run(self.cost, feed_dict = {self.LR:learning_rate,self.X1:self.predictors_scaled[:,0:1],self.X2:self.predictors_scaled[:,1:2],self.Y:self.response_scaled})
-                    print("Epoch:", epoch , "Cost:", c, "Slope1:", session.run(self.slope1),"Slope2:",session.run(self.slope2), "Intercept:", session.run(self.intercept))
-            c = session.run(self.cost, feed_dict = {self.LR: learning_rate,self.X1 : self.predictors_scaled[:,0:1],self.X2 : self.predictors_scaled[:,1:2], self.Y : self.response_scaled} )
-            print("Epoch:", epoch , "Cost:", c, "Slope1:", session.run(self.slope1),"Slope2:",session.run(self.slope2), "Intercept:", session.run(self.intercept))
+                    c = session.run(
+                            self.cost, feed_dict = {
+                                self.LR:learning_rate,
+                                self.X1:self.predictors_scaled[:,0:1],
+                                self.X2:self.predictors_scaled[:,1:2],
+                                self.Y:self.response_scaled})
+                    print("Epoch:", epoch , "Cost:", c, "Slope1:", 
+                            session.run(self.slope1),
+                            "Slope2:",
+                            session.run(self.slope2), 
+                            "Intercept:", 
+                            session.run(self.intercept))
+            c = session.run(
+                    self.cost, feed_dict = {
+                        self.LR: learning_rate,
+                        self.X1 : self.predictors_scaled[:,0:1],
+                        self.X2 : self.predictors_scaled[:,1:2], 
+                        self.Y : self.response_scaled} )
+            print("Epoch:", epoch , "Cost:", c, "Slope1:", 
+                    session.run(self.slope1),
+                    "Slope2:",
+                    session.run(self.slope2), 
+                    "Intercept:", 
+                    session.run(self.intercept))
+            
             return c,session.run(self.slope1),session.run(self.slope2),session.run(self.intercept)
 
 
@@ -147,8 +178,9 @@ def main():
     assert ("%.8f" % slope[0]) == "3.54942311"
     assert ("%.15f" % intercept) == "14.841075232789862"
 
-    print("Problem 1 - Tensorflow")
-    print("======================")
+    print("\nProblem 1, solution 1 - Tensorflow")
+    print("====================================")
+    print("   Epoch 50000, Learning rate 0.0001")
     problem1 = Problem1TF()
     problem1.create_dataset()
     problem1.build()
@@ -158,6 +190,9 @@ def main():
     assert ("%.7f" % slope[0]) == "3.5775077"
     assert ("%.6f" % intercept[0]) == "14.184714"
 
+    print("\nProblem 1, solution 2 - Tensorflow")
+    print("====================================")
+    print("   Epoch 5000, Learning rate 0.001")
     problem1 = Problem1TF()
     problem1.create_dataset()
     problem1.build()
@@ -176,6 +211,7 @@ def main():
 
     print("Problem 2 - Tensorflow")
     print("======================")
+    print("   Epoch 1000000, Learning rate 0.01")
     problem2 = Problem2TF()
     problem2.create_dataset()
     problem2.build()
