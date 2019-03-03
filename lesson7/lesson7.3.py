6aca9ef06b8c785f8ac767d0ce249aeaecc1c1ba
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-03-03 16:28:33 +0000

    separate filter + combine

--- lesson7/lesson7.3.py
+++ lesson7/lesson7.3.py
@@ -13,13 +13,17 @@ misc.imsave("filtered_lady_start.png",im)
 image_gr = im.convert("L")
 arr = np.asarray(image_gr)
 
+filtr = np.genfromtxt('filter2.csv',delimiter=",")
+grad = signal.convolve2d(arr,filtr, mode='same', boundary='symm')
+misc.imsave("filtered_lady_2.png",np.absolute(grad))
+
 filtr = np.genfromtxt('filter1.csv',delimiter=",")
 grad = signal.convolve2d(arr,filtr, mode='same', boundary='symm')
 misc.imsave("filtered_lady_1.png",np.absolute(grad))
 
 filtr = np.genfromtxt('filter2.csv',delimiter=",")
 grad = signal.convolve2d(grad,filtr, mode='same', boundary='symm')
-misc.imsave("filtered_lady_2.png",np.absolute(grad))
+misc.imsave("filtered_lady_3.png",np.absolute(grad))
 
 #fig, aux = plt.subplots(figsize=(512,512)) 
 #fig, aux = plt.subplots() 
