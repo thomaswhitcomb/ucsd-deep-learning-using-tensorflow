7a5ec992a268427c7087c17192e42ab9df4ae63f
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-03-04 05:29:28 +0000

    15x15

--- lesson7/lesson7.3.py
+++ lesson7/lesson7.3.py
@@ -6,20 +6,17 @@ from PIL import Image
 import scipy.ndimage.filters
 
 im = Image.open('01 Lady.png')
-fig, aux = plt.subplots(figsize=(512,512)) 
-fig, aux = plt.subplots() 
-aux.imshow(im, cmap='gray')
 misc.imsave("filtered_lady_start.png",im)
 
 image_gr = im.convert("L")
-arr = np.asarray(image_gr)
+image_array = np.asarray(image_gr)
 
 filtr = np.genfromtxt('filter2.csv',delimiter=",")
-grad = signal.convolve2d(arr,filtr, mode='same', boundary='symm')
+grad = signal.convolve2d(image_array,filtr, mode='same', boundary='symm')
 misc.imsave("filtered_lady_2.png",np.absolute(grad))
 
 filtr = np.genfromtxt('filter1.csv',delimiter=",")
-grad = signal.convolve2d(arr,filtr, mode='same', boundary='symm')
+grad = signal.convolve2d(image_array,filtr, mode='same', boundary='symm')
 misc.imsave("filtered_lady_1.png",np.absolute(grad))
 
 filtr = np.genfromtxt('filter2.csv',delimiter=",")
@@ -33,5 +30,26 @@ filtr_5x5 = np.array([
     [-1,  1,  2,  1, -1],
     [-1, -1, -1, -1, -1]
 ])
-grad = signal.convolve2d(arr,filtr_5x5, mode='same', boundary='symm')
+grad = signal.convolve2d(image_array,filtr_5x5, mode='same', boundary='symm')
 misc.imsave("filtered_lady_5x5.png",np.absolute(grad))
+
+filtr_15x15 = np.array([
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
+
+grad = signal.convolve2d(image_array,filtr_15x15, mode='same', boundary='symm')
+misc.imsave("filtered_lady_15x15.png",np.absolute(grad))
+
