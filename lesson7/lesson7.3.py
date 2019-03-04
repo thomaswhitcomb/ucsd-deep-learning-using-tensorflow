59e4e62aad1abdb769a25fd81ddafbfb952888bc
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-03-04 04:54:38 +0000

    5x5

--- lesson7/lesson7.3.py
+++ lesson7/lesson7.3.py
@@ -3,6 +3,7 @@ from scipy import signal
 from scipy import misc
 import matplotlib.pyplot as plt 
 from PIL import Image
+import scipy.ndimage.filters
 
 im = Image.open('01 Lady.png')
 fig, aux = plt.subplots(figsize=(512,512)) 
@@ -25,6 +26,12 @@ filtr = np.genfromtxt('filter2.csv',delimiter=",")
 grad = signal.convolve2d(grad,filtr, mode='same', boundary='symm')
 misc.imsave("filtered_lady_3.png",np.absolute(grad))
 
-#fig, aux = plt.subplots(figsize=(512,512)) 
-#fig, aux = plt.subplots() 
-#aux.imshow(np.absolute(grad), cmap='gray')
+filtr_5x5 = np.array([
+    [-1, -1, -1, -1, -1],
+    [-1,  1,  2,  1, -1],
+    [-1,  2,  4,  2, -1],
+    [-1,  1,  2,  1, -1],
+    [-1, -1, -1, -1, -1]
+])
+grad = signal.convolve2d(arr,filtr_5x5, mode='same', boundary='symm')
+misc.imsave("filtered_lady_5x5.png",np.absolute(grad))
