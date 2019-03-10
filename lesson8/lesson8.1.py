aa1d573c2f04f5a34187cfa13823399a79576e23
Tom Whitcomb <thomaswhitcomb@gmail.com>
2019-03-10 04:42:37 +0000

    first plot results

--- lesson8/lesson8.1.py
+++ lesson8/lesson8.1.py
@@ -5,39 +5,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import sys
 
 from keras.models import Sequential
 from keras.layers import LSTM
 
+scale = 100
+seq_size = 6
+epochs = 500
 #################################################
 # Create dataset for RNN
 #
-DataList = [[ [i+j] for i in range(5)] for j in range (100)]
-type(DataList)
-DataList[0:5]
-DataList[95:100]
+DataList = [[ [i+j] for i in range(seq_size)] for j in range (scale)]
 
-TargetList = [(i+5) for i in range(100)]
-type(TargetList)
-TargetList[0:5]
-TargetList[95:100]
+TargetList = [(i+seq_size) for i in range(scale)]
 
 data = np.array(DataList, dtype = float)
-data[0:5]
-data.shape
 
 target = np.array(TargetList, dtype = float)
-target[0:5]
-target.shape
-
 ###################################################
 # Scale the data between 0 and 1
 #
-dataScale = data/100
-dataScale[0:2]
+dataScale = data/scale
+print(dataScale[0:2])
 
-targetScale = target/100
-targetScale[0:2]
+targetScale = target/scale
+print(targetScale[0:2])
 
 ##################################################
 # Split data set
@@ -45,18 +38,12 @@ targetScale[0:2]
 #x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=4)
 x_train, x_test, y_train, y_test = train_test_split(dataScale, targetScale, test_size=0.2, random_state=4)
 
-x_train[0:2]
-y_train[0:2]
-
-x_test[0:2]
-y_test[0:2]
-
 ##################################################
 # RNN Model
 #
 model = Sequential()
 # Add the LSTM
-model.add(LSTM((1), batch_input_shape=(None,5,1), return_sequences=False))
+model.add(LSTM((1), batch_input_shape=(None,seq_size,1), return_sequences=False))
 
 model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['accuracy'])
 
@@ -66,18 +53,18 @@ model.summary()
 # Fit the training data to the model
 # Measure the accuracy with test data
 #
-history = model.fit(x_train, y_train, epochs=500, validation_data=(x_test,y_test))
+history = model.fit(x_train, y_train, epochs=epochs, validation_data=(x_test,y_test))
 
 ####################################################
 # Predict using Testing data
 #
 results = model.predict(x_test)
-
-plt.scatter(range(20), results,c='r')
-plt.scatter(range(20), y_test, c='g')
+plt.scatter(range(len(x_test)), results,c='r')
+plt.scatter(range(len(x_test)), y_test, c='g')
 
 ####################################################
 # Plot the loss Function
 #
 plt.plot(history.history['loss'])
+plt.savefig("lesson8.png")
 
